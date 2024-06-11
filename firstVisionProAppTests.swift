799d699a056ba0652f0aa7089345f2042135d1e9
River Camacho <74436549+river82594@users.noreply.github.com>
2024-06-11 18:15:20 -0500

    Add files via upload

---
+++ firstVisionProAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  firstVisionProAppTests.swift
+//  firstVisionProAppTests
+//
+//  Created by River Camacho on 6/11/24.
+//
+
+import Testing
+@testable import firstVisionProApp
+
+struct firstVisionProAppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
